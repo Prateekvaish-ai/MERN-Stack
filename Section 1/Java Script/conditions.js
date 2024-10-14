116bbf4bfb187cf7d2d345383fb7fdbdc3bf88ba
Prateekvaish-ai <prateekvaish449@gmail.com>
2024-10-14 17:30:54 +0530

    programs write js

---
+++ Section 1/Java Script/conditions.js
@@ -0,0 +1,45 @@
+num = 234;
+
+if ( num%2 == 0){
+
+    console.log('even');
+}else{
+
+    console.log('odd');
+
+}
+
+// WAP to check if person is eligible for DL
+
+age = 20;
+
+if ( age >= 18){
+
+    abc = 'something';
+    let a = 'local';
+    console.log(a);
+    const pi = 3.14;
+    pi = 3.15;
+
+    console.log('this is eligible for DL');
+}else{
+    console.log('this is not eligible for DL');
+}
+
+console.log(abc);
+// console.log(a);
+
+
+// WAP to check if a number positive, negative or zero
+
+num = 8;
+
+if ( num >= 0) {
+
+    console.log('positive');
+}else if ( num >= 0){
+    console.log('negative');
+}else if ( num >= 0){
+    console.log('zero');
+}
+
--- Section 1/Java Script/index.js
+++ Section 1/Java Script/index.js
@@ -1,1 +1,15 @@
 console.log('Hello JavaScript');
+
+a = 234;
+
+console.log(a);
+
+myname = 'Pratik';
+
+console.log(myname);
+
+console.log(typeof myname);
+
+console.log(typeof a);
+
+console.log(1 == 1);
